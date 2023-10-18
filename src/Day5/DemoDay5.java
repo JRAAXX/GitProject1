667754b7e9d0a4685611a208b68d388af2d44b3f
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-18 19:59:15 -0400

    added new class

--- src/Day5/DemoDay5.java
+++ src/Day5/DemoDay5.java
@@ -3,7 +3,10 @@ package Day5;
 public class DemoDay5 {
 	
 	public static void main (String [] args) {
-		System.out.println("Today is Synny...");
+		System.out.println("Today is Synny....");
+	}
+	public void getInfo() {
+		System.out.println("Today is java day..");
 	}
 
 }
