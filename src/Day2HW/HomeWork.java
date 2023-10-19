0347ab8a168b94fd06d6d6587d7b55d600e40526
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-19 14:01:50 -0400

    added new code

--- src/Day2HW/HomeWork.java
+++ src/Day2HW/HomeWork.java
@@ -6,9 +6,9 @@ public class HomeWork {
 	public static void main (String [] args) {
 		System.out.println("Day 2 home work");
 	}
-
-
-	public void getInfo () {
-		System.out.println("Today is wonderful");
+	
+	public void getData() {
+		System.out.println ("This is data..");
+		}
 
 }
