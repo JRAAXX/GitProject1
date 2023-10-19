050b91d363c937c06e337659ae5103e4b8ae910b
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-19 14:54:00 -0400

    Hello!

--- src/Day3HMW/homeworkDay3.java
+++ src/Day3HMW/homeworkDay3.java
@@ -7,5 +7,7 @@ public class homeworkDay3 {
 	public static void main (String [] args) {
 		System.out.println("Day 3 home work");
 	}
-
+	public void getInfo () {
+		System.out.println("Today is Tuesday" );
+}
 }
